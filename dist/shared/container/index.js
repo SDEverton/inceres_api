2ac99840639d050ac9857227518bb5273fd713e2
Everton Oliveira <everton.belem@hotmail.com>
2021-05-06 09:46:19 -0300

    Delete dist directory

--- dist/shared/container/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-require("./providers");
-
-var _PrescriptionsRepository = require("../../modules/prescriptions/infra/typeorm/repositories/PrescriptionsRepository");
-
-_tsyringe.container.registerSingleton('PrescriptionsRepository', _PrescriptionsRepository.PrescriptionsRepository);
--- dist/shared/container/providers/CacheProvider/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _RedisCacheProvider = require("./implementations/RedisCacheProvider");
-
-_tsyringe.container.registerSingleton('CacheProvider', _RedisCacheProvider.RedisCacheProvider);
